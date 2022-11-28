d1a88143d5f471b094a573ba28dc0e9099fb66e7
Sreejith Krishnan R <hello.sreejith@pm.me>
2022-11-28 14:52:24 +0530

    Cleanup navigation magic functions

--- magic_functions.py
+++ magic_functions.py
@@ -1,9 +1,16 @@
-import binaryninja as bn
+import logging
 
-from logging import error
+from typing import Optional
+
+import binaryninja as bn
+import binaryninjaui as bnui
 
 from IPython.core.magic import Magics, magics_class, line_magic
 from .user_ns import _BinjaMagicVariablesProvider
+
+
+class _NavMagicError(Exception):
+    pass
 
 
 @magics_class
@@ -13,85 +20,60 @@ class NavMagic(Magics):
         super().__init__(*args, **kwargs)
         self._binja_ns = _BinjaMagicVariablesProvider()
 
+    @classmethod
+    def _parse_int(cls, arg: str, min_val: Optional[int] = None, max_val: Optional[int] = None) -> int:
+        try:
+            result = int(arg)
+        except ValueError:
+            raise _NavMagicError(f'arg "{arg}" must be an integer')
+        if min_val is not None and result < min_val:
+            raise _NavMagicError(f'arg "{arg}" must be at least {min_val}')
+        if max_val is not None and result > max_val:
+            raise _NavMagicError(f'arg "{arg}" must be less than {max_val}')
+        return result
+
+    def _navi(self, arg: str):
+        fn = self._binja_ns.current_il_function
+        if fn is None:
+            raise _NavMagicError(f'current_il_function is None')
+        instr_index = self._parse_int(arg, min_val=0, max_val=len(fn)-1)
+        vl = self._binja_ns.current_ui_view_location
+        if vl is None:
+            raise _NavMagicError(f'current_ui_view_location is None')
+        vl.setInstrIndex(instr_index)
+        self._update_view_location(vl)
+
     @line_magic
     def navi(self, parameter_s=''):
         try:
-            instr_index = int(parameter_s)
-        except ValueError:
-            error(f'invalid arg "{parameter_s}", instruction index must be integer')
-            return
+            self._navi(parameter_s)
+        except _NavMagicError as e:
+            logging.error(e)
 
-        if instr_index < 0:
-            error(f'invalid arg "{instr_index}", instruction index must be positive integer')
-            return
-
-        fn = self._binja_ns.current_il_function
-        if fn is None:
-            error(f'current view should be LLIL, MLIL or HLIL (or its variants)')
-            return
-
-        if instr_index > len(fn):
-            error(f'invalid arg "{instr_index}", instruction index greater than max index {len(fn)}')
-            return
-
-        vl = self._binja_ns.current_ui_view_location
-        if vl is None:
-            error(f'failed to get current view location')
-            return
-
-        vl.setInstrIndex(instr_index)
-        view = self._binja_ns.current_ui_view
-        view.navigateToViewLocation(vl, False)
-
-    def _nav_to_il_block(self, fn: bn.ILFunctionType, block_index: int):
-        if block_index >= len(fn.basic_blocks):
-            error(f'invalid arg "{block_index}", block index must be <= {len(fn.basic_blocks)}')
-            return
-        instr_index = fn.basic_blocks[block_index].start
-        vl = self._binja_ns.current_ui_view_location
-        if vl is None:
-            error(f'failed to get current view location')
-            return
-
-        vl.setInstrIndex(instr_index)
-        view = self._binja_ns.current_ui_view
-        view.navigateToViewLocation(vl, False)
-
-    def _nav_to_fn_block(self, fn: bn.Function, block_index: int):
-        if block_index >= len(fn.basic_blocks):
-            error(f'invalid arg "{block_index}", block index must be <= {len(fn.basic_blocks)}')
-            return
-        offset = fn.basic_blocks[block_index].start
-        vl = self._binja_ns.current_ui_view_location
-        if vl is None:
-            error(f'failed to get current view location')
-            return
-
-        vl.setOffset(offset)
-        view = self._binja_ns.current_ui_view
-        view.navigateToViewLocation(vl, False)
+    def _navb(self, arg: str):
+        current_function = self._binja_ns.current_il_function or self._binja_ns.current_function
+        if current_function is None:
+            raise _NavMagicError(f'current_il_function and current_function is None')
+        block_index = self._parse_int(arg, min_val=0, max_val=len(current_function.basic_blocks) - 1)
+        view_location = self._binja_ns.current_ui_view_location
+        if view_location is None:
+            raise _NavMagicError(f'current_ui_view_location is None')
+        if isinstance(current_function, bn.Function):
+            view_location.setOffset(current_function.basic_blocks[block_index].start)
+        else:
+            view_location.setInstrIndex(current_function.basic_blocks[block_index].start)
+        self._update_view_location(view_location)
 
     @line_magic
     def navb(self, parameter_s=''):
         try:
-            block_index = int(parameter_s)
-        except ValueError:
-            error(f'invalid arg "{parameter_s}", block index must be integer')
-            return
+            self._navb(parameter_s)
+        except _NavMagicError as e:
+            logging.error(e)
 
-        if block_index < 0:
-            error(f'invalid arg "{block_index}", block index must be positive integer')
-            return
-
-        fn = self._binja_ns.current_il_function
-        if fn is not None:
-            self._nav_to_il_block(fn, block_index)
-            return
-
-        fn = self._binja_ns.current_function
-        if fn is not None:
-            self._nav_to_fn_block(fn, block_index)
-            return
-
-        error('failed to get current function')
-    
+    def _update_view_location(self, vl: bnui.ViewLocation) -> bool:
+        view = self._binja_ns.current_ui_view
+        if view is None:
+            return False
+        bn.execute_on_main_thread_and_wait(lambda: view.navigateToViewLocation(vl))
+        return True
